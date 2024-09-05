6e1df81b10cf1ecfef663552dca3afc488628731
Shravani GR <shravanigr20@gmail.com>
2024-09-05 20:16:49 +0530

    3. Allow the add method to handle new lines between numbers (instead of commas). ("1\n2,3" should return 6)

--- Assessment.java
+++ Assessment.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class Assessment {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter a string of comma-separated numbers");
+        System.out.println("Enter a string numbers");
         String string = sc.nextLine();
         Calculator obj = new Calculator();
         int result = obj.add(string);
@@ -13,12 +13,14 @@ public class Assessment {
 
 class Calculator {
     public int add(String input) {
+        if (input.isEmpty()) {
+            return 0;
+        }
         int sum = 0;
-        String[] numbers = input.split(",");
+        String[] numbers = input.split("[,\n]");
         int[] ints = new int[numbers.length];
-        for (int i = 0; i < numbers.length; i++) {
-            ints[i] = Integer.parseInt(numbers[i]);
-            sum = sum + ints[i];
+        for (String num : numbers) {
+            sum += Integer.parseInt(num);
         }
         return sum;
     }
